59d815aa7a6af621dabfd07fd5f3c45bbc8621b8
DevBritto <davidbrittoo99@gmail.com>
2023-01-17 15:08:18 -0300

    Conclusão da parte de JS e tambem do projeto

--- scripts.js
+++ scripts.js
@@ -1,24 +1,51 @@
 const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
 
 const dolar = 5.2
+const euro = 5.9
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
 
-    realValueText.innerHTML = inputReais
+
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
     ).format(inputReais);
 
 
+    if (select.value === 'US$ Dólar americano') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar);
+    }
 
-    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
-        { style: 'currency', currency: 'USD' }
-    ).format(inputReais / dolar);
+    if (select.value === '€ Euro') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReais / euro);
+    }
 
 }
-button.addEventListener('click', convertValues)
+changeCurrency = () => {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-image')
 
+    if (select.value === 'US$ Dólar americano') {
+        currencyName.innerHTML = 'Dolar americano'
+        currencyImg.src = './estados-unidos (1) 1.svg'
+    }
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImg.src = './Euro.svg'
+    }
+    convertValues()
+
+}
+
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
