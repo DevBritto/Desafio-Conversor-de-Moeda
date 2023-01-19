82ff97efabeba153998d8b57e7b4cb447d33788a
DevBritto <davidbrittoo99@gmail.com>
2023-01-19 16:07:11 -0300

    Conclusão do preojeto

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const select = document.getElementById('currency-select')
 
 const dolar = 5.2
 const euro = 5.9
+const bitcoin = 109000
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
@@ -27,7 +28,12 @@ const convertValues = () => {
             { style: 'currency', currency: 'EUR' }
         ).format(inputReais / euro);
     }
-
+    
+if (select.value === 'Bitcoin') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+           { style: 'decimal', currency: 'XBT' }
+       ).format(inputReais / bitcoin.toFixed(08))
+}     
 }
 changeCurrency = () => {
     const currencyName = document.getElementById('currency-name')
@@ -41,6 +47,11 @@ changeCurrency = () => {
     if (select.value === '€ Euro') {
         currencyName.innerHTML = 'Euro'
         currencyImg.src = './Euro.svg'
+    }
+
+    if (select.value === 'Bitcoin') {
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = './Bitcoin.png'
     }
     convertValues()
 
