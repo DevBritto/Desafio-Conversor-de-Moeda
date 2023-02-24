4ff6a71e3f1d81f4e39b109fe99a9646682f835d
DevBritto <davidbrittoo99@gmail.com>
2023-02-23 22:58:34 -0300

    conclusão adicionando a api contendo os valores em tempo real

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.2
-const euro = 5.9
-const bitcoin = 109000
-
-const convertValues = () => {
+const convertValues =  async () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
 
+const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+    console.log(data)
 
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
@@ -32,7 +33,7 @@ const convertValues = () => {
 if (select.value === 'Bitcoin') {
         currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
            { style: 'decimal', currency: 'XBT' }
-       ).format(inputReais / bitcoin.toFixed(08))
+       ).format(inputReais / bitcoin)      
 }     
 }
 changeCurrency = () => {
